7422c065b8a10124f941b9bce4b237df67fc95c8
dentex <samuele.rini76@gmail.com>
2013-01-28 12:38:09 -0500

    add Licences - wip2

---
+++ dentex.youtube.downloader/src/dentex/youtube/downloader/GplShowActivity.java
@@ -0,0 +1,14 @@
+package dentex.youtube.downloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GplShowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_gpl_show);
+	}
+
+}
---
+++ dentex.youtube.downloader/src/dentex/youtube/downloader/MitShowActivity.java
@@ -0,0 +1,14 @@
+package dentex.youtube.downloader;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MitShowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_mit_show);
+	}
+
+}
